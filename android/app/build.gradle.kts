9fe0ff4d8e728a017d3c0244ac8349e6441fdd10
Vishnupriyan <vishnupriyan466@gmail.com>
2026-03-19 14:03:27 +0530

    App running on mobile - Firebase connected

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
@@ -26,6 +26,11 @@ android {
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
+        externalNativeBuild {
+            cmake {
+                version = "3.31.4"
+            }
+        }
     }
 
     buildTypes {
